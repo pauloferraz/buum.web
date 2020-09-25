edb4343f1c6818f18b3e9d26d62c5440137386bc
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-09-25 08:17:10 -0300

    test: ensure Login calls validation with correct values

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,29 +1,53 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import light from '@/presentation/theme/light'
 import Login from '.'
 
+import { Validation } from '@/presentation/protocols/validation'
+
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+  validade(input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
   const sut = render(
     <ThemeProvider theme={light}>
-      <Login/>
+      <Login validation={validationSpy}/>
     </ThemeProvider>
   )
-  return { sut }
+  return { sut, validationSpy }
 }
 
 describe('Login Component', () => {
-  test('should be not showing error and loading', () => {
+  afterEach(cleanup)
+
+  test('should start with initial state', () => {
     const { sut } = makeSut()
     const statusWrap = sut.getByTestId('status-wrap')
     expect(statusWrap.childElementCount).toBe(0)
 
     const submitButton = sut.getByTestId('submit-button') as HTMLButtonElement
     expect(submitButton.disabled).toBeTruthy()
+  })
+
+  test('should call Validation with correct value', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
   })
 })
