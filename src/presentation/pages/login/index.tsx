be5bfaf2040bf8ad647a2eb8194770cb0824c410
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-09-28 21:48:34 -0300

    feat: ensure Login calls Authentication with correct values

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -3,6 +3,7 @@ import FormStatus from '@/presentation/components/form-status'
 import Input from '@/presentation/components/input'
 import Context from '@/presentation/contexts/form-context'
 import { Validation } from '@/presentation/protocols/validation'
+import { Authentication } from '@/domain/usecases'
 
 import {
   Container,
@@ -16,9 +17,10 @@ import {
 
 type Props = {
   validation: Validation
+  authentication: Authentication
 }
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
@@ -42,9 +44,10 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     }
   }, [state.email, state.password])
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
     setState({ ...state, isLoading: true })
+    await authentication.auth({ email: state.email, password: state.password })
   }
 
   return (
