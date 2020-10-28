276ece8d69f7d837c6d535916ab2f2637536adc1
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-10-28 20:40:19 -0300

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,9 +1,17 @@
-import { RequiredFieldValidation } from '@/validation/validators'
+import {
+  RequiredFieldValidation,
+  EmailValidation
+} from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   test('should return RequiredFieldValidation', () => {
     const validators = sut.field('any_field').required().build()
     expect(validators).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('should return EmailValidation', () => {
+    const validators = sut.field('any_field').email().build()
+    expect(validators).toEqual([new EmailValidation('any_field')])
   })
 })
