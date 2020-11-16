61d2b5a1d1d49017d1688e6adc9cf7a1892084c3
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-11-15 21:07:12 -0300

    feat: ensure Signup calls AddAccount with correct values

--- src/presentation/pages/signup/index.tsx
+++ src/presentation/pages/signup/index.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import FormStatus from '@/presentation/components/form-status'
 import Input from '@/presentation/components/input'
 import Context from '@/presentation/contexts/form-context'
+import { Validation } from '@/presentation/protocols/validation'
+import { AddAccount } from '@/domain/usecases'
 
 import {
   Container,
@@ -14,7 +16,12 @@ import {
   LinkCreate
 } from './styles'
 
-const Signup: React.FC = () => {
+type Props = {
+  validation: Validation
+  addAccount: AddAccount
+}
+
+const Signup: React.FC<Props> = ({ validation, addAccount }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     name: '',
@@ -26,13 +33,55 @@ const Signup: React.FC = () => {
     btnDisabled: true
   })
 
+  useEffect(() => {
+    if (
+      !validation.validade('email', state.email) &&
+      !validation.validade('password', state.password)
+    ) {
+      setState({
+        ...state,
+        btnDisabled: false
+      })
+    } else {
+      setState({
+        ...state,
+        btnDisabled: true
+      })
+    }
+  }, [state.email, state.password])
+
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault()
+
+    try {
+      if (state.isLoading) {
+        return
+      }
+      setState({ ...state, isLoading: true })
+      await addAccount.add({
+        name: state.name,
+        email: state.email,
+        password: state.password,
+        passwordConfirmation: state.passwordConfirmation
+      })
+    } catch (error) {
+      setState({ ...state, isLoading: false, errorMessage: error.message })
+    }
+  }
+
   return (
     <Container>
       <LoginContainer>
         <Context.Provider value={{ state, setState }}>
           <Title>Portal do vendedor</Title>
           <SubTitle>Gerencie sua loja de forma fácil e rápida</SubTitle>
-          <FormLogin autoComplete='off' onSubmit={() => {}} data-testid='login-form'>
+          <FormLogin
+            autoComplete='off'
+            onSubmit={handleSubmit}
+            data-testid='signup-form'
+          >
             <Input name='name' placeholder='Digite seu nome' />
             <Input name='email' placeholder='Digite seu e-mail' />
             <Input type='password' name='password' placeholder='Digite sua senha' />
