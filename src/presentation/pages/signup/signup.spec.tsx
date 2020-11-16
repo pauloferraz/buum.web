9254e9f8a31b280e7a4eedde50d5c2fa6aa507e3
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-11-15 21:07:24 -0300

    test: ensure Signup calls AddAccount with correct values

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,34 +1,143 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import {
+  cleanup,
+  fireEvent,
+  render,
+  RenderResult,
+  waitFor
+} from '@testing-library/react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
 import { ThemeProvider } from 'styled-components'
+import faker from 'faker'
 import light from '@/presentation/theme/light'
 import Signup from '.'
+import { AddAccountSpy, ValidationStub } from '@/presentation/test'
 
 type SutTypes = {
   sut: RenderResult
+  validationStub: ValidationStub
+  addAccountSpy: AddAccountSpy
 }
 
 const history = createMemoryHistory({ initialEntries: ['/signup'] })
 const makeSut = (): SutTypes => {
+  const validationStub = new ValidationStub()
+  const addAccountSpy = new AddAccountSpy()
   const sut = render(
     <Router history={history}>
       <ThemeProvider theme={light}>
-        <Signup />
+        <Signup validation={validationStub} addAccount={addAccountSpy} />
       </ThemeProvider>
     </Router>
   )
-  return { sut }
+  return { sut, validationStub, addAccountSpy }
+}
+
+const populateNameField = (sut: RenderResult, name = faker.random.word()): void => {
+  const nameInput = sut.getByTestId('name')
+  fireEvent.input(nameInput, { target: { value: name } })
+}
+
+const populateEmailField = (
+  sut: RenderResult,
+  email = faker.internet.email()
+): void => {
+  const emailInput = sut.getByTestId('email')
+  fireEvent.input(emailInput, { target: { value: email } })
+}
+
+const populatePasswordField = (
+  sut: RenderResult,
+  password = faker.internet.password()
+): void => {
+  const passwordInput = sut.getByTestId('password')
+  fireEvent.input(passwordInput, { target: { value: password } })
+}
+
+const populatePasswordConfirmationField = (
+  sut: RenderResult,
+  password = faker.internet.password()
+): void => {
+  const passwordInput = sut.getByTestId('passwordConfirmation')
+  fireEvent.input(passwordInput, { target: { value: password } })
+}
+
+const simulateValidSubmit = async (
+  sut: RenderResult,
+  name = faker.random.words(),
+  email = faker.internet.email(),
+  password = faker.internet.password(),
+  passwordConfirmation = faker.internet.password()
+): Promise<void> => {
+  populateNameField(sut, name)
+  populateEmailField(sut, email)
+  populatePasswordField(sut, password)
+  populatePasswordConfirmationField(sut, passwordConfirmation)
+  const form = sut.getByTestId('signup-form')
+  fireEvent.submit(form)
+  await waitFor(() => form)
 }
 
 describe('Signup component', () => {
+  afterEach(cleanup)
+
   test('should start with initial state', () => {
     const { sut } = makeSut()
     const statusWrap = sut.getByTestId('status-wrap')
     expect(statusWrap.childElementCount).toBe(0)
 
-    const submitButton = sut.getByTestId('submit-button') as HTMLButtonElement
-    expect(submitButton.disabled).toBeTruthy()
+    /* const submitButton = sut.getByTestId('submit-button') as HTMLButtonElement
+    expect(submitButton.disabled).toBeTruthy() */
+  })
+
+  test('should show name valid name state if Validation succeeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    populateNameField(sut)
+    const errorMessage = sut.getByTestId('nameError')
+    expect(errorMessage.textContent.length).toBe(0)
+  })
+
+  test('should show email valid password state if Validation succeeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    populateEmailField(sut)
+    const errorMessage = sut.getByTestId('emailError')
+    expect(errorMessage.textContent.length).toBe(0)
+  })
+
+  test('should enable submit button if form is valid', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    populateEmailField(sut)
+    populateNameField(sut)
+    populatePasswordField(sut)
+    const submitBtn = sut.getByTestId('submit-button') as HTMLButtonElement
+    expect(submitBtn.disabled).toBe(false)
+  })
+
+  test('should enable spinner on form submit', async () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    await simulateValidSubmit(sut)
+    const spinner = sut.getByTestId('status-wrap')
+    expect(spinner).toBeTruthy()
+  })
+
+  test('should call AddAccount with correct values', async () => {
+    const { sut, validationStub, addAccountSpy } = makeSut()
+    validationStub.errorMessage = null
+    const name = faker.name.findName()
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    const passwordConfirmation = faker.internet.password()
+    await simulateValidSubmit(sut, name, email, password, passwordConfirmation)
+    expect(addAccountSpy.params).toEqual({
+      name,
+      email,
+      password,
+      passwordConfirmation
+    })
   })
 })
