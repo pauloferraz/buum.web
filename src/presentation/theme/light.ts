2eeeab1e0a7dae5c63b181c430e4227c854c7210
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-11-30 19:06:16 -0300

    chore: remove travis and coverals

--- src/presentation/theme/light.ts
+++ src/presentation/theme/light.ts
@@ -2,7 +2,7 @@ export default {
   title: 'light',
   colors: {
     primary: '#6c5dd3',
-    secondary: '#0049c6',
+    secondary: '#A0D7E7',
     accent_1: '#ffa2c0',
     accent_2: '#ffce73',
     alert: '#FF754C',
