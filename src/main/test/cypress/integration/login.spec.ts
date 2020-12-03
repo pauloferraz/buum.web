3951437833f6df59db788d9a1b4cb4eeee7d4495
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-12-03 19:00:19 -0300

    test: cy ensure Login begin with correct values

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,11 @@
 describe('Login', () => {
-  it('should load correct values', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('should load correct values', () => {
+    cy.getByTestId('submit-button').should('be.disabled')
+    cy.getByTestId('email').should('be.empty')
+    cy.getByTestId('password').should('be.empty')
+    cy.getByTestId('status-wrap').should('not.have.descendants')
   })
 })
