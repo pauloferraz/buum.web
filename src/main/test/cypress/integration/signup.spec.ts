77d3548d01e8d162e2cc52fe4f327504d0810e35
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-12-09 17:47:39 -0300

    test: cy ensure Sigunp present error if email is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 describe('Signup', () => {
   beforeEach(() => {
     cy.visit('signup')
@@ -9,5 +11,15 @@ describe('Signup', () => {
     cy.getByTestId('password').should('be.empty')
     cy.getByTestId('passwordConfirmation').should('be.empty')
     cy.getByTestId('status-wrap').should('not.have.descendants')
+  })
+
+  it('should present error state if email is invalid', () => {
+    cy.getByTestId('name').type(faker.random.words())
+    cy.getByTestId('email').type(faker.random.word())
+    const pass = faker.random.alphaNumeric(6)
+    cy.getByTestId('password').type(pass)
+    cy.getByTestId('passwordConfirmation').type(pass)
+    cy.getByTestId('status-wrap').should('not.have.descendants')
+    cy.getByTestId('submit-button').should('be.disabled')
   })
 })
