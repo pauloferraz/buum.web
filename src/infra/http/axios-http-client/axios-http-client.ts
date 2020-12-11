7f5ef595314e82e44d7d8f4f62a78ce0c0bacfad
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-12-11 17:57:06 -0300

    feat: ensure AxiosHttpClient calls axios.get with correct url

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,17 +1,26 @@
-import { HttpPostParams, HttpResponse, HttpPostClient } from '@/data/protocols/http'
+import {
+  HttpPostParams,
+  HttpResponse,
+  HttpPostClient,
+  HttpGetParams
+} from '@/data/protocols/http'
 import axios, { AxiosResponse } from 'axios'
 
 export class AxiosHttpClient implements HttpPostClient {
   async post(params: HttpPostParams): Promise<HttpResponse> {
-    let httpResponse: AxiosResponse
+    let axiosResponse: AxiosResponse
     try {
-      httpResponse = await axios.post(params.url, params.body)
+      axiosResponse = await axios.post(params.url, params.body)
     } catch (error) {
-      httpResponse = error.response
+      axiosResponse = error.response
     }
     return {
-      statusCode: httpResponse.status,
-      body: httpResponse.data
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
     }
+  }
+
+  async get(params: HttpGetParams): Promise<void> {
+    await axios.get(params.url)
   }
 }
