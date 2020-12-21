c8e39905909964ff64697dabe8b2d16e6385b5fc
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-12-21 15:44:33 -0300

    test: ensure SurveyList page call LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -3,13 +3,31 @@ import { render, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import light from '@/presentation/theme/light'
 import SurveyList from '.'
+import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
+import { SurveyModel } from '@/domain/models'
 
-const makeSut = (): void => {
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+  async loadAll(): Promise<SurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+
   render(
     <ThemeProvider theme={light}>
-      <SurveyList />
+      <SurveyList loadSurveyList={loadSurveyListSpy} />
     </ThemeProvider>
   )
+
+  return { loadSurveyListSpy }
 }
 
 describe('SurveyList Page', () => {
@@ -17,5 +35,10 @@ describe('SurveyList Page', () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-content')
     expect(surveyList.querySelectorAll('div').length).toBe(4)
+  })
+
+  test('Should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
