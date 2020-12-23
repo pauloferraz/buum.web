97971b12d0dc3ab34a7f9508fa8c6fe9f3b796ff
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2020-12-22 21:01:54 -0300

    feat: ensure SurveyList render SurveyError on error

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
+import { SurveyModel } from '@/domain/models'
 
 import Sidebar from '@/presentation/components/sidebar'
 import Header from '@/presentation/components/header'
@@ -7,7 +8,6 @@ import {
   SurveyItem,
   SurveyItemEmpty
 } from '@/presentation/pages/survey-list/components'
-import { SurveyModel } from '@/domain/models'
 
 import {
   PageWrap,
@@ -23,11 +23,15 @@ type Props = {
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   const [state, setState] = useState({
-    surveys: [] as SurveyModel[]
+    surveys: [] as SurveyModel[],
+    error: ''
   })
 
   useEffect(() => {
-    loadSurveyList.loadAll().then(surveys => setState({ surveys }))
+    loadSurveyList
+      .loadAll()
+      .then(surveys => setState({ ...state, surveys }))
+      .catch(error => setState({ ...state, error: error.message }))
   }, [])
 
   return (
@@ -39,12 +43,21 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
           <SurveyWrap>
             <PageTitle>Minhas Enquetes</PageTitle>
             <SurveyContent data-testid='survey-content'>
-              {state.surveys.length ? (
-                state.surveys.map((survey: SurveyModel) => (
-                  <SurveyItem key={survey.id} survey={survey} />
-                ))
+              {state.error.length ? (
+                <div data-testid='survey-error'>
+                  <span>{state.error}</span>
+                  <button>Recarregar</button>
+                </div>
               ) : (
-                <SurveyItemEmpty />
+                <>
+                  {state.surveys.length ? (
+                    state.surveys.map((survey: SurveyModel) => (
+                      <SurveyItem key={survey.id} survey={survey} />
+                    ))
+                  ) : (
+                    <SurveyItemEmpty />
+                  )}
+                </>
               )}
             </SurveyContent>
           </SurveyWrap>
