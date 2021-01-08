f37319e1addb61344073cc2ead180b13f9ba2881
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-01-08 17:38:37 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -4,7 +4,6 @@ import faker from 'faker'
 export const mockSurveyModel = (): SurveyModel => ({
   id: faker.random.uuid(),
   question: faker.random.words(10),
-  answers: [{ answer: faker.random.words(4) }, { answer: faker.random.words(6) }],
   didAnswer: faker.random.boolean(),
   date: faker.date.recent()
 })
--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,7 +1,11 @@
-import { makeAxiosHttpClient, makeApiUrl } from '@/main/factories/http'
+import { makeApiUrl } from '@/main/factories/http'
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-load-survey-list'
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
+import { makeAuthorizeHttpGetClientDecorator } from '../../decorator'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveyList(
+    makeApiUrl('/surveys'),
+    makeAuthorizeHttpGetClientDecorator()
+  )
 }
