066bb041145095f3e7243e13408c6afa5aaba027
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-01-14 21:13:34 -0300

    refactor: move sut to a factory method

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -1,27 +1,41 @@
 import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import light from '@/presentation/theme/light'
 import { Header } from '@/presentation/components'
 import { ApiContext } from '@/presentation/contexts'
+import { AccountModel } from '@/domain/models'
+
+type SutTypes = {
+  history: MemoryHistory
+  setCurrentAccountMock: (account: AccountModel) => void
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  const setCurrentAccountMock = jest.fn()
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
+      <Router history={history}>
+        <ThemeProvider theme={light}>
+          <Header />
+        </ThemeProvider>
+      </Router>
+    </ApiContext.Provider>
+  )
+  return {
+    history,
+    setCurrentAccountMock
+  }
+}
 
 describe('Header component', () => {
   test('should call setCurrentAccount with null', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-    const sutCurrentAccountMock = jest.fn()
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: sutCurrentAccountMock }}>
-        <Router history={history}>
-          <ThemeProvider theme={light}>
-            <Header />
-          </ThemeProvider>
-        </Router>
-      </ApiContext.Provider>
-    )
+    const { history, setCurrentAccountMock } = makeSut()
     fireEvent.click(screen.getByTestId('logout'))
-    expect(sutCurrentAccountMock).toHaveBeenCalledWith(undefined)
+    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
     expect(history.location.pathname).toBe('/login')
   })
 })
