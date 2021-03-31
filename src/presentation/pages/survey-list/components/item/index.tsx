c6558b042c5432346381e2d2e8bebc446436e453
Paulo Ferraz <dev.paulo.ferraz@gmail.com>
2021-03-30 22:24:47 -0300

    feat: ensure SurveyList redirect to SurveyResult

--- src/presentation/pages/survey-list/components/item/index.tsx
+++ src/presentation/pages/survey-list/components/item/index.tsx
@@ -1,5 +1,6 @@
 import { SurveyModel } from '@/domain/models'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import {
   SurveyItemWrap,
@@ -16,18 +17,22 @@ type Props = {
 const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
   return (
     <SurveyItemWrap data-testid='survey-item'>
-      <SurveyDate>
-        <SurveyDateMini data-testid='month'>
-          {survey.date.toLocaleString('pt-BR', { month: 'short' }).replace('.', '')}
-        </SurveyDateMini>
-        <SurveyDateText data-testid='day'>
-          {survey.date.getDate().toString().padStart(2, '0')}
-        </SurveyDateText>
-        <SurveyDateMini data-testid='year'>
-          {survey.date.getFullYear()}
-        </SurveyDateMini>
-      </SurveyDate>
-      <SurveyTitle data-testid='question'>{survey.question}</SurveyTitle>
+      <Link to={`/surveys/${survey.id}`} data-testid='survey-link'>
+        <SurveyDate>
+          <SurveyDateMini data-testid='month'>
+            {survey.date
+              .toLocaleString('pt-BR', { month: 'short' })
+              .replace('.', '')}
+          </SurveyDateMini>
+          <SurveyDateText data-testid='day'>
+            {survey.date.getDate().toString().padStart(2, '0')}
+          </SurveyDateText>
+          <SurveyDateMini data-testid='year'>
+            {survey.date.getFullYear()}
+          </SurveyDateMini>
+        </SurveyDate>
+        <SurveyTitle data-testid='question'>{survey.question}</SurveyTitle>
+      </Link>
     </SurveyItemWrap>
   )
 }
